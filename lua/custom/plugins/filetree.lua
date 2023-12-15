5b4d033f25ee5b339960b2092bea3cedfb2f1fc8
Ivailo Hristov <crownandthering@gmail.com>
2023-12-15 14:50:58 +0200

    Map o for open in the file tree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -10,6 +10,64 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function ()
-    require('neo-tree').setup {}
+    require('neo-tree').setup({
+      window = {
+        position = "left",
+        width = 40,
+        mapping_options = {
+          noremap = true,
+          nowait = true,
+        },
+        mappings = {
+          ["o"] = "open",
+          ["oc"] = false,
+          ["od"] = false,
+          ["og"] = false,
+          ["om"] = false,
+          ["on"] = false,
+          ["os"] = false,
+          ["ot"] = false,
+        }
+      },
+      filesystem = {
+        window = {
+          mappings = {
+            ["oc"] = false,
+            ["od"] = false,
+            ["og"] = false,
+            ["om"] = false,
+            ["on"] = false,
+            ["os"] = false,
+            ["ot"] = false,
+          }
+        },
+      },
+      buffers = {
+        window = {
+          mappings = {
+            ["oc"] = false,
+            ["od"] = false,
+            ["og"] = false,
+            ["om"] = false,
+            ["on"] = false,
+            ["os"] = false,
+            ["ot"] = false,
+          }
+        },
+      },
+      git_status = {
+        window = {
+          mappings = {
+            ["oc"] = false,
+            ["od"] = false,
+            ["og"] = false,
+            ["om"] = false,
+            ["on"] = false,
+            ["os"] = false,
+            ["ot"] = false,
+          }
+        },
+      },
+    })
   end,
 }
