3f4b335790248a4ad2314035dcc2ea8929b23d61
Ivailo Hristov <crownandthering@gmail.com>
2026-02-16 11:54:02 +0200

    Setup pint for PHP

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -5,10 +5,11 @@ return {
       formatters_by_ft = {
         javascript = { "prettierd" },
         typescript = { "prettierd" },
+        php = { "pint" },
       },
       format_on_save = {
         -- These options will be passed to conform.format()
-        timeout_ms = 500,
+        timeout_ms = 1000,
         lsp_format = "never",
       },
     })
